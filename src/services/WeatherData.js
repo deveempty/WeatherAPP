af161c480abbd0dd72431bcc6ee82e118152f813
deveempty <juanviloriav@gmail.com>
2021-07-05 16:13:35 -0500

    This is not working yet. Sorry lol

---
+++ src/services/WeatherData.js
@@ -0,0 +1,6 @@
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const API_BASE = "https://api.openweathermap.org/data/2.5/";
+
+const weatherData = async (id) => {
+    const apiURL = id ? 
+}
